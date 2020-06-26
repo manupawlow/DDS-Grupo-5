70dda1dd4d6ac82755179d3d003ec1df881a413d
Gen13673 <gen13673@hotmail.com>
2020-06-25 22:52:50 -0300

    ingreso y criterio

---
+++ TP ANUAL DDS/TP ANUAL DDS/Egresos/Criterio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_ANUAL_DDS.Egresos
+{
+    public class Criterio
+    {
+        public string descripcion;
+        public string jerarquia;
+        public Categoria categoria;
+    }
+}
---
+++ TP ANUAL DDS/TP ANUAL DDS/Egresos/Ingreso.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_ANUAL_DDS.Egresos
+{
+    public class Ingreso
+    {
+
+    }
+}
